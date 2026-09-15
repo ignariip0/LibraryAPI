e49ab5cab9e71a5a9d48ac87c49147ef9410cd2d
Cauê <cauepbrito0@gmail.com>
2026-09-15 18:11:52 -0300

    Adicionando validação de campos com Bean Validation

--- src/main/java/com/github/ignariip0/libraryapi/controller/AutorController.java
+++ src/main/java/com/github/ignariip0/libraryapi/controller/AutorController.java
@@ -6,6 +6,7 @@ import com.github.ignariip0.libraryapi.exceptions.OperacaoNaoPermitidaException;
 import com.github.ignariip0.libraryapi.exceptions.RegistroDuplicadoException;
 import com.github.ignariip0.libraryapi.model.Autor;
 import com.github.ignariip0.libraryapi.service.AutorService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -27,7 +28,7 @@ public class AutorController {
     private final AutorService service;
     
     @PostMapping
-    public ResponseEntity<Object> salvar(@RequestBody AutorDTO autor){
+    public ResponseEntity<Object> salvar(@RequestBody @Valid AutorDTO autor){
         try {
             Autor autorEntidade = autor.mapearParaAutor();
             service.salvar(autorEntidade);
